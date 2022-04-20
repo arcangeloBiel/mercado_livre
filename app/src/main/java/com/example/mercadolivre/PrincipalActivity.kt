3c290c131ba618cadabfc9935b1ebe29e4f30bc0
joao <joao.ribeiro@havan.com.br>
2022-04-20 09:00:32 -0300

    third commit

--- app/src/main/java/com/example/mercadolivre/PrincipalActivity.kt
+++ app/src/main/java/com/example/mercadolivre/PrincipalActivity.kt
@@ -4,14 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class PrincipalActivity : AppCompatActivity() {
 
+    lateinit var slider: ImageSlider
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+        inicializarslider()
+    }
+
+    fun inicializarslider() {
+        slider = findViewById(R.id.image_slider)
+        val listaImagens = ArrayList<SlideModel>()
+        listaImagens.add(SlideModel(R.drawable.promocao01, ScaleTypes.CENTER_CROP))
+        listaImagens.add(SlideModel(R.drawable.promocao02, ScaleTypes.CENTER_CROP))
+        listaImagens.add(SlideModel(R.drawable.promocao03, ScaleTypes.CENTER_CROP))
+        slider.setImageList(listaImagens)
     }
 
     fun deslogar(view: View) {
